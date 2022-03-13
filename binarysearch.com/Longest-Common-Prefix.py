09623a8c5de991f97dc105e5ccc8d02e6d4e655e
amirshokh <amirshoxmuxitdinov@gmail.com>
2022-03-13 22:14:31 +0500

    Longest-Common-Prefix.py

---
+++ binarysearch.com/Longest-Common-Prefix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def solve(self, words):
+        prefix = ""
+        words = sorted(words)
+        for word in words:
+            pass
+        return words
+
+
+s = Solution()
+print(s.solve(['as', 'asdasd', 'a']))
