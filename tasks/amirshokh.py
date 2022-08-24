1d09ad8957a9ff05db803eeedd7779964725eda2
Amirshox Muxitdinov <amirshoxmuxitdinov@gmail.com>
2022-08-24 16:00:01 +0300

    contest

--- tasks/amirshokh.py
+++ tasks/amirshokh.py
@@ -1,41 +1,14 @@
-INT_HEX = {
-    0: "0",
-    1: "1",
-    2: "2",
-    3: "3",
-    4: "4",
-    5: "5",
-    6: "6",
-    7: "7",
-    8: "8",
-    9: "9",
-    10: "A",
-    11: "B",
-    12: "C",
-    13: "D",
-    14: "E",
-    15: "F",
-}
+def fifo(n, reference_list):
+    index = 0
+    result = [-1] * n
 
+    for num in reference_list:
+        if num not in result:
+            if index >= n:
+                index = 0
+            result[index] = num
+            index += 1
+        else:
+            continue
 
-def max_or_min(num):
-    if num > 255:
-        return 255
-    elif num < 0:
-        return 0
-    return num
-
-
-def int_to_hex(num):
-    return INT_HEX[num // 16] + INT_HEX[num % 16]
-
-
-def rgb(r, g, b):
-    r = max_or_min(r)
-    g = max_or_min(g)
-    b = max_or_min(b)
-
-    return int_to_hex(r) + int_to_hex(g) + int_to_hex(b)
-
-
-print(rgb(255, 255, 255))
+    return result
--- tasks/main.py
+++ tasks/main.py
@@ -1,43 +1,8 @@
-letter_string = 'abcdefghijklmnopqrstuvwxyz'
-letters = [
-    'aa',
-    'ab',
-    'ac',
-    'ad',
-    'ae',
-    'af',
-    'ag',
-    'ah',
-    'ai',
-    'aj',
-    'ak',
-    'al',
-    'am',
-    'an',
-    'ao',
-    'ap',
-    'aq',
-    'ar',
-    'as',
-    'at',
-    'au',
-    'av',
-    'aw',
-    'ax',
-    'ay',
-    'az',
-]
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
 
-full_letters = []
 
-for letter in letters:
-
-    for second_index in list(range(1, 26)):
-
-        for index in list(range(0, 26)):
-            full_letters.append(letter_string[index] + letter)
-
-        letter = letter_string[second_index] + letter[-1]
-
-    for index in list(range(0, 26)):
-        full_letters.append(letter_string[index] + letter_string[-1] + letter[-1])
+print(fib(10))
